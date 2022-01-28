2fab0848745ffae0b174cf2ee501feb0bff20627
alexis jimenez <87094396+alexhizjimenez@users.noreply.github.com>
2022-01-28 17:00:08 -0600

    master

---
+++ database/migrations/2022_01_28_211735_create_user_domicilio_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserDomicilioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_domicilio', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->text('domicilio');
+            $table->string('numero_exterior');
+            $table->string('colonia');
+            $table->string('cp');
+            $table->string('ciudad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_domicilio');
+    }
+}
